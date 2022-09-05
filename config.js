1ed9bce284fa8b5289d5bbd6b83747a9218ca8d3
Kanak Rav <95073658+KanakRav@users.noreply.github.com>
2022-09-05 16:08:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+ import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDqmry4OEHPX0BpSrjm0jYlXD18fcyK2A",
+  authDomain: "class-attendance-app-3d330.firebaseapp.com",
+  projectId: "class-attendance-app-3d330",
+  storageBucket: "class-attendance-app-3d330.appspot.com",
+  messagingSenderId: "806067062241",
+  appId: "1:806067062241:web:bf773846dc776b08497ea0"
+};
+
+
+  export default firebase.database()
+ 
+
+  
